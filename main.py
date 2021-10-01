3652ebbc04d7f0a240e45bc2296ee866ad02bf76
Samuel Soler <ss@bb.vision>
2021-10-01 13:29:46 -0300

    Carpetas

---
+++ main.py
@@ -0,0 +1,5 @@
+from scripts.amazon        import scraping 
+from scripts.appletv       import scraping 
+from scripts.disneyplus    import scraping 
+from scripts.hbo           import scraping 
+from scripts.netflix       import scraping 
